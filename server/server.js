a82c7e51b14db5a19842824e10b77fe6cbcaabdd
Sarah Wimark <sarahwimark@gmail.com>
2019-05-17 12:58:30 +0200

    Adding .gitignore

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const path = require('path')
 const express = require('express')
 
+const port = process.env.PORT || 3000
+
 const app = express()
 
 app.use(express.static(path.join(__dirname, '/../public')))
 
-app.listen(3000, () => {
-  console.log('Server listening on http://localhost:3000')
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`)
 })
